21458c09844f034a2ed32ec785dd21f60ebff272
carrieguo <593774767@qq.com>
2020-04-10 11:14:21 +0800

    vuex 安装，简易demo

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>count: {{this.$store.state.count}}</h1>
+    <button @click="countIncrease">点击赋值</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  methods: {
+    countIncrease() {
+      const v = 100;
+      this.$store.commit('countIncrease', v);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+
+Vue.config.productionTip = false
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    countIncrease(state, v) {
+      state.count = v;
+    }
+  }
+})
+
+new Vue({
+  store,
+  render: h => h(App),
+}).$mount('#app')
